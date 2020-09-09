031690f150b8a39bcd0fa739f97bfa5553f5e706
ranjeet-tech <ranjeet.lanting@student.rocvf.nl>
2020-09-09 11:26:34 +0200

    change tekst from "hallo klas" 8a to "hallo mensen"

---
+++ src/h02/applet.java
@@ -0,0 +1,16 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class applet extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Ranjeet lanting", 50,  50 );
+    }
+}
